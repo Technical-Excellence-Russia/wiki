77c09c35301a500654e40aa7f6987203b5fff53d
timmson <timmson666@mail.ru>
2021-01-20 20:33:54 +0300

    Add content generator #3

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("./lib/fs");
+const Logger = require("./lib/logger");
+
+const rootFileName = __dirname + "/" + "README.md";
+const EOL = "\r\n";
+
+const logger = new Logger();
+logger.info("File spellchecking and generation:");
+
+let readMeContents = [];
+readMeContents.push("# Wiki");
+readMeContents.push("");
+readMeContents.push("## Оглавление");
+readMeContents.push("");
+readMeContents = readMeContents.concat(fs.getListOfMDFiles(__dirname).map(filename => `[${filename.split(".")[0]}](${filename})`));
+readMeContents.push("");
+readMeContents.push("Помочь проекту [Technical-Excellence-Russia/wiki](https://github.com/Technical-Excellence-Russia/wiki)");
+readMeContents.push("");
+readMeContents.push("Copyright (c) 2021 Technical Excellence Russia");
+
+let startTime = new Date().getTime();
+fs.writeFileContent(rootFileName, readMeContents.join(EOL))
+    .then(() => logger.infoFile(rootFileName, startTime))
+    .catch(e => logger.error(e));
+
