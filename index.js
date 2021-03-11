369175d5f84e407b00eddb6fbcbcf431b395c113
Krotov Artem <timmson666@mail.ru>
2021-03-11 14:22:51 +0300

    Add sitemap (#34)
    * Add sitemap generation
    
    * Generate index
    
    Co-authored-by: wiki[bot] <wiki[bot]@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,25 +2,36 @@ const FS = require("./lib/fs");
 const Parser = require("./lib/parser");
 const Logger = require("./lib/logger");
 const Mustache = require("mustache");
+const {SitemapStream, streamToPromise} = require("sitemap");
+const {Readable} = require("stream");
 
 const fs = new FS();
 const parser = new Parser(fs);
 const logger = new Logger();
 
+const url = "https://technical-excellence.ru/wiki";
 const rootFileName = __dirname + "/" + "README.md";
+const sitemapFileName = __dirname + "/" + "sitemap.xml";
+
 
 logger.info("File generation:");
+const articles = parser.filesToArticles(__dirname);
 
 const data = {
     currentYear: new Date().getFullYear(),
-    articles: parser.filesToContent(__dirname)
+    articles: parser.articlesToContent(articles)
 }
 
+// Generate README.md
 const template = fs.readFile(rootFileName + ".template");
-
 let startTime = new Date().getTime();
 const content = Mustache.render(template, data);
-fs.writeFileContent(rootFileName, content)
-    .then(() => logger.infoFile(rootFileName, startTime))
-    .catch(e => logger.error(e));
+fs.writeFileContent(rootFileName, content).then(() => logger.infoFile(rootFileName, startTime)).catch(e => logger.error(e));
 
+//Generate sitemap.xml
+const links = articles.map((a) => new Object({url: url + "/" + a.fileName, lastmod: a.updated, changefreq: "monthly"}));
+console.log(articles);
+startTime = new Date().getTime();
+streamToPromise(Readable.from(links).pipe(new SitemapStream({hostname: url}))).then((content) =>
+    fs.writeFileContent(sitemapFileName, content).then(() => logger.infoFile(sitemapFileName, startTime)).catch(e => logger.error(e))
+);
--- test/test-parser.js
+++ test/test-parser.js
@@ -10,8 +10,12 @@ class FS {
         return Object.keys(this.content);
     }
 
+    getModificationDate(fileName) {
+        return this.content[fileName].modificationDate
+    }
+
     readFile(fileName) {
-        return this.content[fileName];
+        return this.content[fileName].content;
     }
 
 }
@@ -20,9 +24,9 @@ class FS {
 describe("Parser should", () => {
 
     const fs = new FS({
-            "fileOne.md": "# Complete Article\r\nSome Content",
-            "fileTwo.md": "# Incomplete Article [draft]\r\nSome Content",
-            "fileThree.md": "# Other Article\r\nSome Content"
+            "fileOne.md": {content: "# Complete Article\r\nSome Content", modificationDate: "2008-10-25"},
+            "fileTwo.md": {content: "# Incomplete Article [draft]\r\nSome Content", modificationDate: "2008-10-26"},
+            "fileThree.md": {content: "# Other Article\r\nSome Content", modificationDate: "2008-12-25"}
         }
     );
     const parser = new Parser(fs);
@@ -34,7 +38,7 @@ describe("Parser should", () => {
     });
 
     test("return table of contents", () => {
-        const result = parser.filesToContent(".");
+        const result = parser.articlesToContent(parser.filesToArticles("."));
 
         expect(result).toHaveLength(2);
     });
