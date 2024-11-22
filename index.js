3cd38219c91b0e1915c83963619652c276352644
Artem Krotov <timmson666@mail.ru>
2024-11-22 12:24:53 +0300

    Some refactoring (#65)
    * Some refactoring
    
    * Fix tests

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const FS = require("./lib/fs");
-const Parser = require("./lib/parser");
-const Logger = require("./lib/logger");
-const Mustache = require("mustache");
-const {SitemapStream, streamToPromise} = require("sitemap");
-const {Readable} = require("stream");
+const FS = require("./lib/fs")
+const Parser = require("./lib/parser")
+const Logger = require("./lib/logger")
+const Mustache = require("mustache")
+const {SitemapStream, streamToPromise} = require("sitemap")
+const {Readable} = require("stream")
 
-const fs = new FS();
-const parser = new Parser(fs);
-const logger = new Logger();
+const fs = new FS()
+const parser = new Parser(fs)
+const logger = new Logger()
 
-const url = "https://technical-excellence.ru/wiki";
-const rootFileName = __dirname + "/" + "README.md";
-const sitemapFileName = __dirname + "/" + "sitemap.xml";
+const url = "https://technical-excellence.ru/wiki"
+const rootFileName = __dirname + "/docs/" + "README.md"
+const sitemapFileName = __dirname + "/docs/" + "sitemap.xml"
 
 
-logger.info("File generation:");
-const articles = parser.filesToArticles(__dirname);
+logger.info("File generation:")
+const articles = parser.filesToArticles(__dirname + "/docs")
 
 const data = {
     currentYear: new Date().getFullYear(),
@@ -23,14 +23,18 @@ const data = {
 }
 
 // Generate README.md
-const template = fs.readFile(rootFileName + ".template");
-let startTime = new Date().getTime();
-const content = Mustache.render(template, data);
-fs.writeFileContent(rootFileName, content).then(() => logger.infoFile(rootFileName, startTime)).catch(e => logger.error(e));
+const template = fs.readFile(rootFileName + ".template")
+let startTime = new Date().getTime()
+const content = Mustache.render(template, data)
+fs.writeFileContent(rootFileName, content).then(() => logger.infoFile(rootFileName, startTime)).catch(e => logger.error(e))
 
 //Generate sitemap.xml
-const links = articles.map((a) => new Object({url: url + "/" + a.fileName.replace(".md", ".html"), lastmod: a.updated, changefreq: "monthly"}));
-startTime = new Date().getTime();
+const links = articles.map((a) => new Object({
+    url: url + "/" + a.fileName.replace(".md", ".html"),
+    lastmod: a.updated,
+    changefreq: "monthly"
+}));
+startTime = new Date().getTime()
 streamToPromise(Readable.from(links).pipe(new SitemapStream({hostname: url}))).then((content) =>
     fs.writeFileContent(sitemapFileName, content).then(() => logger.infoFile(sitemapFileName, startTime)).catch(e => logger.error(e))
-);
+)
--- test/test-parser.js
+++ test/test-parser.js
@@ -1,21 +1,21 @@
-const Parser = require("../lib/parser");
+const Parser = require("../lib/parser")
 
 class FS {
 
     constructor(content) {
-        this.content = content;
+        this.content = content
     }
 
     getListOfMDFiles() {
-        return Object.keys(this.content);
+        return this.content.map((it) => it.fileName)
     }
 
     getModificationDate(fileName) {
-        return this.content[fileName].modificationDate
+        return this.content.filter((it) => fileName.indexOf(it.fileName) >= 0)[0].modificationDate
     }
 
     readFile(fileName) {
-        return this.content[fileName].content;
+        return this.content.filter((it) => fileName.indexOf(it.fileName) >= 0)[0].content
     }
 
 }
@@ -23,24 +23,36 @@ class FS {
 
 describe("Parser should", () => {
 
-    const fs = new FS({
-            "fileOne.md": {content: "# Complete Article\r\nSome Content", modificationDate: "2008-10-25"},
-            "fileTwo.md": {content: "# Incomplete Article [draft]\r\nSome Content", modificationDate: "2008-10-26"},
-            "fileThree.md": {content: "# Other Article\r\nSome Content", modificationDate: "2008-12-25"}
-        }
-    );
+    const fs = new FS([
+            {
+                fileName: "fileOne.md",
+                content: "# Complete Article\r\nSome Content",
+                modificationDate: "2008-10-25"
+            },
+            {
+                fileName: "fileTwo.md",
+                content: "# Incomplete Article [draft]\r\nSome Content",
+                modificationDate: "2008-10-26"
+            },
+            {
+                fileName: "fileThree.md",
+                content: "# Other Article\r\nSome Content",
+                modificationDate: "2008-12-25"
+            }
+        ]
+    )
     const parser = new Parser(fs);
 
     test("return Article Name", () => {
-        const result = parser.getArticleName("fileThree.md");
+        const result = parser.getArticleName("fileThree.md")
 
-        expect(result).toEqual("Other Article");
+        expect(result).toEqual("Other Article")
     });
 
     test("return table of contents", () => {
-        const result = parser.articlesToContent(parser.filesToArticles("."));
+        const result = parser.articlesToContent(parser.filesToArticles("."))
 
-        expect(result).toHaveLength(2);
+        expect(result).toHaveLength(2)
     });
 
 });
